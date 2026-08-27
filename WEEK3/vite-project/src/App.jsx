294a310cbbab800a13a966d63d4fb3020407c1d4
Douglas-Morris <douglas.morris888@gmail.com>
2026-08-27 22:48:05 +1000

    yteas

--- WEEK3/vite-project/src/App.jsx
+++ WEEK3/vite-project/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react'
 import Footer from './components/Footer'
-import axios from 'axios'
 import Note from './components/Note'
 import noteService from './services/notes'
 import Notification from './components/Notification'
@@ -21,14 +20,13 @@ const App = () => {
   }, [])
 
   const toggleImportanceOf = (id) => {
-    const url = `http://localhost:3001/notes/${id}`
     const note = notes.find(n => n.id === id)
     const changedNote = {...note, important: !note.important}
 
     noteService
       .update(id, changedNote)
       .then(returnedNote => {
-        setNotes(notes.map(note => note.id === id ? returnedNote.data : note))
+        setNotes(notes.map(note => note.id === id ? returnedNote : note))
       })
       .catch(error => {
         setErrorMessage(
--- week4-backend/index.js
+++ week4-backend/index.js
@@ -78,6 +78,22 @@ app.post('/api/notes', (request, response) => {
     
     response.json(note)
 })
+
+app.put('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  const note = notes.find(note => note.id === id)
+
+  if (!note) {
+    return response.status(404).end()
+  }
+
+  const body = request.body
+  note.content = body.content
+  note.important = body.important
+
+  response.json(note)
+})
+
 app.delete('/api/notes/:id', (request, response) => {
   const id = request.params.id
   notes = notes.filter(note => note.id !== id)
