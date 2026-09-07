278d7b5edf86a5f8008b3ff66196aa1d91ffc3ad
Douglas-Morris <douglas.morris888@gmail.com>
2026-09-07 22:10:49 +1000

    j

--- week4-backend/index.js
+++ week4-backend/index.js
@@ -1,5 +1,31 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://douglasmorris888_db_user:${password}@cluster0.t0tvqne.mongodb.net/noteApp?appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url, { family: 4 })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
 
 app.use(express.static('dist'))
 
@@ -37,7 +63,9 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
