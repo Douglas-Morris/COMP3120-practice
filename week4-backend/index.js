29a29142f769d94771689a1a951ccb4a5fa22f3a
Douglas-Morris <douglas.morris888@gmail.com>
2026-09-10 20:13:59 +1000

    eslint fixes

--- week4-backend/index.js
+++ week4-backend/index.js
@@ -4,7 +4,7 @@ const app = express()
 const mongoose = require('mongoose')
 const Note = require('./models/note')
 
-const password = process.argv[2]
+//const password = process.argv[2]
 
 // const url = `mongodb+srv://douglasmorris888_db_user:${password}@cluster0.t0tvqne.mongodb.net/noteApp?appName=Cluster0`
 
@@ -12,13 +12,13 @@ mongoose.set('strictQuery',false)
 
 //mongoose.connect(url, { family: 4 })
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
+// const noteSchema = new mongoose.Schema({
+//   content: String,
+//   important: Boolean,
+// })
 
 
-let notes = []
+//let notes = []
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -55,27 +55,27 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.post('/api/notes', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
-    
-    const note = new Note({
-      content: body.content,
-      important: body.important || false,
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
     })
+  }
 
-    note.save().then(savedNote => {
-      response.json(savedNote)
-    })
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
     .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const {content, important} = request.body
+  const { content, important } = request.body
 
   Note.findById(request.params.id)
     .then(note => {
@@ -94,7 +94,7 @@ app.put('/api/notes/:id', (request, response, next) => {
 
 app.delete('/api/notes/:id', (request, response, next) => {
   Note.findByIdAndDelete(request.params.id)
-    .then(result => {
+    .then(() => {
       response.status(204).end()
     })
     .catch(error => next(error))
